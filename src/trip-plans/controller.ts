9c78e44313f07dc16a2784bf4271b764071b125b
big-foot-dev <fernando.hernandez@plugdevs.com>
2024-03-20 11:14:12 -0600

    feat(tip-plan): Create feature "save a trip plan"

--- src/trip-plans/controller.ts
+++ src/trip-plans/controller.ts
@@ -1,6 +1,7 @@
 import {  type Request, type Response } from 'express'
 import { tripPlanService } from './services'
 import { ITripPlan } from '../models/ITripPlan'
+import { IUser } from '../models/ILog'
 
 export default {
     createTripPlan: async(req: Request, res:Response) => { 
@@ -52,6 +53,52 @@ export default {
                     }
                 })
             }
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                error: {
+                    message: `Error in the request: ${error.message}`,
+                    detail: error.message,
+                    code: "INTERNAL_SERVER_ERROR",
+                }
+            })
+        }
+    },
+    saveTripPlan: async (req: Request, res: Response) => {
+        try {
+            const { tripPlanId, user } : {tripPlanId: string, user:IUser} = req.body
+            if(!tripPlanId) throw new Error('No trip plan id was given')
+            if(!user) throw new Error('No user was given')           
+            const {success, data} = await tripPlanService.getTripPlan(tripPlanId)
+            if(success && data !== undefined){
+                const { places, tripName, tripLocation, dates, people, budget} = data
+                const newTripPlan: ITripPlan = {
+                    tripName,
+                    places,
+                    tripLocation,
+                    dates,
+                    people,
+                    budget,
+                    owner : user,
+                    isPrivate: false,
+                    invitedUsers: [],
+                    interactionRef: ''
+                } 
+                const {success, error, data: tripPlanCreated} = await tripPlanService.createTripPlan(newTripPlan)
+                if(!success) throw new Error(error?.message)
+                return res.status(201).json({
+                    success: true,
+                    data: tripPlanCreated
+                })
+            }else{
+                return res.status(400).json({
+                    success: false,
+                    error: {
+                        message: "Error saving the trip plan"
+                    }
+                })
+            }
+
         } catch (error) {
             res.status(500).json({
                 success: false,
